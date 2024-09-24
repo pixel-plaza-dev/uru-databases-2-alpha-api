a01826202a04a12c667e462ab946d54ddfed5883
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-09-24 09:30:42 -0400

    feat: Added security headers, rate limiting and body parser limit

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
+import dotenv from 'dotenv';
+import { json } from 'express';
+
+dotenv.config({ path: '.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Add security headers
+  app.use(helmet());
+
+  // Add rate limiting
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      limit: 100, // limit each IP to 100 requests per window
+    }),
+  );
+
+  // Body parser limit
+  app.use(json({ limit: '10kb' }));
+
   await app.listen(3000);
 }
+
 bootstrap();
